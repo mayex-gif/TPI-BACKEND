4a9bedb1ff2d45899dfe04f4587f579d10d3d087
mayex-gif <alcaldealejo53@gmail.com>
2025-10-18 02:34:56 -0300

    Creacion del microservicio ms-tarifas

---
+++ ms-tarifas/src/test/java/com/backend/ms_tarifas/MsTarifasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ms_tarifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTarifasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
