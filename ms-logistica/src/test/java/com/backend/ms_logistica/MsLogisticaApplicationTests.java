4dd72895ae060c2ab5ca3b73875995de9b69377f
mayex-gif <alcaldealejo53@gmail.com>
2025-10-18 02:34:46 -0300

    Creacion del microservicio ms-logistica

---
+++ ms-logistica/src/test/java/com/backend/ms_logistica/MsLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ms_logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
